ac3b22a22de9b604872876eeba593535cf682bc2
jchck <hey@justinchick.com>
2015-07-22 20:31:21 -0500

    write a sexy gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
-// Gulp tasks for MNML
+// gulp tasks for jm
 
 // Load plugins
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
+    basswork = require('gulp-basswork'),
+    rename = require('gulp-rename'),
+    css = require('css'),
+    cssnext = require('gulp-cssnext'),
     watch = require('gulp-watch'),
-    prefix = require('gulp-autoprefixer'),
     size = require('gulp-size'),
     rename = require('gulp-rename'),
     imagemin = require('gulp-imagemin'),
-    minifyCSS = require('gulp-minify-css'),
-    sass = require('gulp-sass'),
-    csslint = require('gulp-csslint'),
-    browserSync = require('browser-sync').create('mnml'),
+    browserSync = require('browser-sync'),
     browserReload = browserSync.reload;
 
-
-// Minify all css files in the css directory
-// Run this in the root directory of the project with `gulp minify-css `
-gulp.task('minify-css', function(){
-  gulp.src('./css/mnml.css')
-    .pipe(minifyCSS())
-    .pipe(rename('mnml.min.css'))
-    .pipe(size({gzip:true, showFiles: true}))
-    .pipe(gulp.dest('./css/'));
+gulp.task('css', function() {
+  gulp.src('./src/base.css')
+    .pipe(basswork())
+    .pipe(cssnext({compress: true}))
+    .pipe(rename({basename: 'jm'}))
+    .pipe(size({gzip: false, showFiles: true, title:'basswork css'}))
+    .pipe(size({gzip: true, showFiles: true, title:'basswork gzipped css'}))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream({match: '**/*.css'}));
 });
 
 gulp.task('minify-img', function(){
@@ -34,36 +34,6 @@ gulp.task('minify-img', function(){
     .pipe(gulp.dest('./img/'));
 });
 
-// Use csslint without box-sizing or compatible vendor prefixes (these
-// don't seem to be kept up to date on what to yell about)
-gulp.task('csslint', function(){
-  gulp.src('./css/mnml.css')
-    .pipe(csslint({
-          'compatible-vendor-prefixes': false,
-          'box-sizing': false,
-          'important': false,
-          'known-properties': false
-        }))
-    .pipe(csslint.reporter());
-});
-
-// Task that compiles scss files down to good old css
-gulp.task('pre-process', function(){
-    return gulp.src("./sass/mnml.scss")
-        .pipe(sass())
-        .on('error', swallowError)
-        .pipe(prefix())
-        .pipe(size({gzip: false, showFiles: true}))
-        .pipe(size({gzip: true, showFiles: true}))
-        .pipe(gulp.dest('css'))
-        .pipe(minifyCSS())
-        .pipe(rename('mnml.min.css'))
-        .pipe(size({gzip: false, showFiles: true}))
-        .pipe(size({gzip: true, showFiles: true}))
-        .pipe(gulp.dest('./css/'))
-        .pipe(browserSync.stream({match: '**/*.css'}));
-});
-
 // Initialize browser-sync which starts a static server also allows for
 // browsers to reload on filesave
 gulp.task('browser-sync', function() {
@@ -72,7 +42,7 @@ gulp.task('browser-sync', function() {
     });
 });
 
-// Allows gulp to not break after a sass error.
+// Allows gulp to not break after an error.
 // Spits error out to console
 function swallowError(error) {
   console.log(error.toString());
@@ -92,4 +62,3 @@ gulp.task('default', ['pre-process', 'browser-sync'], function(){
   gulp.watch('sass/*', ['pre-process']);
   gulp.watch('*.html', browserReload);
 });
-
